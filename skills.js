811c1b86452c7d2dfa94d53f51e48716019ccb84
VINICIUS RUBENS PEDRINHO <vrpedrinho@gmail.com>
2025-08-01 19:33:47 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,10 @@
+function calculateNumbers(var1, var2) {
+    return var1 + var2;
+}
+
+module.exports = calculateNumbers;
+// This function takes two numbers as input and returns their sum.
+// It can be used in other files by importing this module.
+// Example usage:
+// const calculateNumbers = require('./skills.js');
+// console.log(calculateNumbers(5, 10)); // Outputs: 15
