9fc8e779f09f43a132b740514ab92d902e0e2793
VINICIUS RUBENS PEDRINHO <vrpedrinho@gmail.com>
2025-08-01 18:04:30 -0300

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,17 +1,1 @@
-// Create web server 
-// to handle HTTP requests
-const http = require('http');
-
-// Define the server port
-const PORT = 3000;
-
-// Create an HTTP server
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, World!\n');
-});
-
-// Start the server
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+// Create web server
